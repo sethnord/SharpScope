892e2596e2c8fb6843c73b1d34bd0d4587fa5e45
Seth Nordahl <sethnord812002@gmail.com>
2020-02-10 16:27:04 -0800

    In progress 2/10 @ 1626

---
+++ SharpScope/ADSB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using SharpScope;
+using System.Threading;
+using System.IO;
+
+namespace SharpScope
+{
+    ///<summary>This class contains all neccesary functions used to process ADS-B data.</summary>
+    ///
+    class ADSB
+    {
+        static TcpClient tcpListener;
+        static Stream inputStream;
+        ///<summary>Creates a TCP connection to an ADS-B data source</summary>
+        ///
+        public static bool Connect()
+        {
+            try
+            {
+                tcpListener = new TcpClient();
+                Console.WriteLine("Connecting...");
+                Console.WriteLine("Target- IP: " + Program.serverIP + " PORT: " + Program.serverPort);
+                tcpListener.Connect(Program.serverIP, Program.serverPort);
+                Console.WriteLine("Connection Acquired.");
+                //TODO: Remove below code and add to its own method.
+                inputStream = tcpListener.GetStream();
+                Thread.Sleep(1000);
+                StreamReader reader = new StreamReader(inputStream);
+                Console.WriteLine(reader.ReadLine());
+                Thread.Sleep(10000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool Close()
+        {
+            try
+            {
+                tcpListener.Close();
+            }catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- SharpScope/Program.cs
+++ SharpScope/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpScope;
+using System.Threading;
 
 namespace SharpScope
 {
     class Program
     {
+        public static string serverIP = "127.0.0.1";
+        public static int serverPort = 30003;
         static void Main(string[] args)
         {
+            //Begin by creating a TCP connection on port 30003 to the ADS-B data source.
+            Console.WriteLine("Establishing connection to ADS-B source...");
+            bool connected = ADSB.Connect();
+            if(connected == true)
+            {
+                Console.WriteLine("Connection successful.");
+            }
+            else
+            {
+                Console.WriteLine("Could not connect.");
+            }
         }
     }
 }
